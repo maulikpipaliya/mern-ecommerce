b1768c94c3fe8bdc0d380b726be7aa1a3ba18712
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-06-25 08:10:35 +0530

    Added Login Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <main className='py-3'>
                 <Container>
                     <Route path='/' component={HomeScreen} exact />
+                    <Route path='/signin' component={LoginScreen} />
                     <Route path='/product/:slugid' component={ProductScreen} />
                     {/* question mark makes it optional */}
                     <Route path='/cart/:slugid?' component={CartScreen} />
