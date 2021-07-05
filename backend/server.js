fedc9db2f5b1920c18b51af9c36ef2b9d1c9aa2f
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2021-07-05 13:52:42 +0530

    Port configured

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log(
         `[ Environment: ${process.env.NODE_ENV} ] : Listening on port ${PORT}`
             .green
